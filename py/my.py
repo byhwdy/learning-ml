d85a80501fbbcca51d270803b3eda58ebc6bd518
byhwdy <byhwdy@gmail.com>
2018-05-21 00:10:05 +0800

    edit

--- py/my.py
+++ py/my.py
@@ -1,2 +1,17 @@
-a = 1
-a
+#### 迭代器
+class my_iterater:
+	def __init__(self):
+		self.lim = 10
+
+	def __iter__(self):
+		return self
+
+	def __next__(self):
+		current_value = self.lim
+		self.lim -= 1
+		if self.lim > 0:
+			return 10 - current_value
+		else:
+			raise StopIteration
+
+			
