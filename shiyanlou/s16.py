c5cbead103b18cf6c5ec969f309ca40c97ef60d0
byhwdy <byhwdy@gmail.com>
2018-06-21 00:39:46 +0800

    edit

--- shiyanlou/s16.py
+++ shiyanlou/s16.py
@@ -20,9 +20,76 @@ def d_euc(x, y):
 	""" 
 	return np.sqrt(np.sum(np.power((x-y), 2)))
 
+def update_center(clusters, data, centers):
+	"""更新中心点
+	参数: 
+	clusters: 每一点分好的类别
+	data： 数据集
+	centers： 中心点集合
+	返回：
+	new_centers.reshape(num_centers, num_features): 新中心点集合
+	"""
+	num_centers = np.shape(centers)[0]  # 中心点个数
+	num_features = np.shape(centers)[1]  # 特征数
+	container = []
+	for x in range(num_centers):
+		each_container = []
+		container.append(each_container)     # 容器，将相同类别数据存放在一起
 
+	for i, cluster in enumerate(clusters):
+		container[cluster].append(data[i])
 
+	# 为方便计算，将list类型 转换为np.array类型
+	container = np.array(list(map(lambda x: np.array(x), container)))
 
+	new_centers = np.array([])  # 创建一个容器，存放中心点的坐标
+	for i in range(len(container)):
+		each_center = np.mean(container[i], axis=0) # 将每一个子集中数据均值作为中心点
+		new_centers = np.append(new_centers, each_center)
+
+	return new_centers.reshape(num_centers, num_features)  # 以矩阵方式返回中心点坐标
+
+def kmeans_cluster(data, init_centers, k):
+	"""K-Means
+	参数：
+	data：数据集
+	init_centers： 初始化中心点集合
+	k：中心点个数
+	返回：
+	centers_container： 每一次更新中心点集合
+	cluster_container：每一次更新类别集合
+	"""
+	max_step = 50  # 定义最大迭代次数，中心点最多移动的次数
+	epsilon = 0.001 # 定义一个足够小的数， 通过中心点变化的距离是否小于该数，判断中心点是否变化
+
+	old_centers = init_centers
+
+	centers_container = []  # 建立一个中心点容器，存放每一次变化后的中心点，以便后面绘图
+	cluster_container = []  # 建立一个类别容器，存放每次中心点变化后的数据的类别
+	centers_container.append(old_centers)
+
+	for step in range(max_step):
+		cluster = np.array([], dtype=int)
+		for each_data in data:
+			distances = np.array([])
+			for each_center in old_centers:
+				temp_distance = d_euc(each_data, each_center)  # 计算样本和中心点的欧氏距离
+				distances = np.append(distances, temp_distance)
+			lab = np.argmin(distances)  # 返回距离最近中心点的索引，即按照最近中心点分类
+			cluster = np.append(cluster, lab)
+		cluster_container.append(cluster)
+
+		new_centers = update_center(cluster, data, old_centers)  # 根据子集分类更新中心点
+
+		difference = np.fabs(new_centers-old_centers)
+
+		if difference.any() < epsilon:  # 判断中心点是否移动
+			return centers_container, cluster_container
+
+		centers_container.append(new_centers)
+		old_centers = new_centers
+
+	return centers_container, cluster_container
 
 
 
@@ -51,9 +118,15 @@ if __name__ == '__main__':
 	# 初始化3个中心点
 	init_centers = random_k(3, blobs)
 	# print(init_centers)
-	
 
+	# 计算最终中心点
+	centers_container, cluster_container = kmeans_cluster(blobs, init_centers, 3)
+	final_center = centers_container[-1]
+	final_cluster = cluster_container[-1]
+	# print(final_cluster.shape)
+	
 	# 画图
-	plt.scatter(blobs[:, 0], blobs[:, 1], s=20)
-	plt.scatter(init_centers[:,0], init_centers[:,1], s=100, marker='*', c='r')
+	plt.scatter(blobs[:, 0], blobs[:, 1], s=20, c=final_cluster)
+	plt.scatter(final_center[:,0], final_center[:,1], s=100, marker='*', c='r')
+
 	plt.show()
